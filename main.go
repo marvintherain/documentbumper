16fbbe9fe164c324212c8b6cdd29f6b6ab4a1799
marvintherain <daniel.markow@posteo.de>
2020-07-24 17:12:35 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main 
+
+import (
+    "fmt"
+    "os"
+	"path/filepath"
+	"time"
+	"syscall"
+)
+
+func main() {
+	var files []string
+	twentyThreeMonthsAgo := time.Now().AddDate(0, -2, 0)
+	// fileCreation := make(map[string]time.Time)
+
+    root := "C:\\Users\\danie\\OneDrive\\sync"
+    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() == false {
+			d := info.Sys().(*syscall.Win32FileAttributeData)
+			cTime := time.Unix(0, d.CreationTime.Nanoseconds())
+			
+			if cTime.Before(twentyThreeMonthsAgo) == true {
+				files = append(files, path)
+
+			}
+			// fileCreation[path] = cTime
+		}
+        return nil
+    })
+    if err != nil {
+        panic(err)
+	}
+	
+	for _, entry := range files {
+		fmt.Println(entry)
+	}
+
+	// fmt.Println(fileCreation)
+
+    // for path, entry := range fileCreation {
+	// 	fmt.Println(path)
+	// 	fmt.Println(entry)
+    // }
+}
